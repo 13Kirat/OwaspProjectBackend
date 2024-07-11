7872a876ee4e60a43b0461bef993c1fa1b4e84af
13Kirat <141816179+13Kirat@users.noreply.github.com>
2024-07-12 02:10:45 +0530

    minor change

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,9 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
 			Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
 		),
 		httpOnly: true, // Set httpOnly to true
-		secure: true, // for https
+		// secure: true, // for https
 	};
-
 	res.status(statusCode).cookie("token", token, options).json({
 		success: true,
 		user,
