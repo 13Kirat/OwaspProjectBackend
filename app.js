72ca2a6108da5046bb68b1b94813792d9ee57673
13Kirat <141816179+13Kirat@users.noreply.github.com>
2024-07-11 22:18:17 +0530

    server setup

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const express = require("express");
+import { config } from "dotenv";
+import cookieParser from "cookie-parser"; // used to set cookies and authentication
+import cors from "cors"; // used too connect frontend and backend
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import express from "express";
+import fileUpload from "express-fileupload";
 
 const app = express();
+config({ path: "./config/config.env" });
 
-const port = 8080;
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URL],
+		method: ["GET", "POST", "DELETE", "PUT"],
+		credentials: true,
+	})
+);
 
-app.listen(port, () => {
-	console.log(`Server started on ${port}`);
-});
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+	fileUpload({
+		// used to upload files like in multer
+		useTempFiles: true,
+		tempFileDir: "/tmp/",
+	})
+);
+
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
