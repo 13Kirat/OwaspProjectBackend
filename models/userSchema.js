cc0ba5df2cabf075a930e3226150965f4109b94f
13Kirat <141816179+13Kirat@users.noreply.github.com>
2024-07-12 16:35:31 +0530

    buffercommand false

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,67 +2,70 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import validator from "validator";
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, "Please enter your Name!"],
-		minLength: [3, "Name must contain at least 3 Characters!"],
-		maxLength: [30, "Name cannot exceed 30 Characters!"],
-	},
-	email: {
-		type: String,
-		required: [true, "Please enter your Email!"],
-		validate: [validator.isEmail, "Please provide a valid Email!"],
-	},
-	phone: {
-		type: Number,
-		required: [true, "Please enter your Phone Number!"],
-	},
-	password: {
-		type: String,
-		required: [true, "Please provide a Password!"],
-		minLength: [8, "Password must contain at least 8 characters!"],
-		maxLength: [32, "Password cannot exceed 32 characters!"],
-		select: false,
-	},
-	role: {
-		type: String,
-		required: [true, "Please select a role"],
-		enum: ["Job Seeker", "Employer", "Organizer", "Admin"],
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-	year: {
-		type: String,
-		required: true,
-	},
-	branch: {
-		type: String,
-		required: true,
-	},
-	collage: {
-		type: String,
-		required: true,
-	},
-	profileImage: {
-		public_id: {
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please enter your Name!"],
+			minLength: [3, "Name must contain at least 3 Characters!"],
+			maxLength: [30, "Name cannot exceed 30 Characters!"],
+		},
+		email: {
+			type: String,
+			required: [true, "Please enter your Email!"],
+			validate: [validator.isEmail, "Please provide a valid Email!"],
+		},
+		phone: {
+			type: Number,
+			required: [true, "Please enter your Phone Number!"],
+		},
+		password: {
+			type: String,
+			required: [true, "Please provide a Password!"],
+			minLength: [8, "Password must contain at least 8 characters!"],
+			maxLength: [32, "Password cannot exceed 32 characters!"],
+			select: false,
+		},
+		role: {
+			type: String,
+			required: [true, "Please select a role"],
+			enum: ["Job Seeker", "Employer", "Organizer", "Admin"],
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		year: {
 			type: String,
 			required: true,
 		},
-		url: {
+		branch: {
 			type: String,
 			required: true,
 		},
-	},
-	events: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Order",
+		collage: {
+			type: String,
+			required: true,
 		},
-	],
-});
+		profileImage: {
+			public_id: {
+				type: String,
+				required: true,
+			},
+			url: {
+				type: String,
+				required: true,
+			},
+		},
+		events: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Order",
+			},
+		],
+	},
+	{ bufferCommands: false }
+);
 
 //ENCRYPTING THE PASSWORD WHEN THE USER REGISTERS OR MODIFIES HIS PASSWORD
 userSchema.pre("save", async function (next) {
