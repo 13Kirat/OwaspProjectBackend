0f9653ab4053040cde8041d50d71c29b6ed478b3
13Kirat <141816179+13Kirat@users.noreply.github.com>
2024-07-13 22:42:03 +0530

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,15 @@ import { catchAsyncErrors } from "./catchAsyncError.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) {
-    return next(new ErrorHandler("User Not Authorized", 401));
-  }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+	const { token } = req.cookies;
+	console.log(req.cookies);
+	console.log(token);
+	if (!token) {
+		return next(new ErrorHandler("User Not Authorized", 401));
+	}
+	const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  req.user = await User.findById(decoded.id);
+	req.user = await User.findById(decoded.id);
 
-  next();
-
+	next();
 });
